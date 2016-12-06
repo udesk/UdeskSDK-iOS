4bca0aabd498b976ef041b9b2e1848a708a61df5
Mac开发机 <Mac@macdeMacBook-Pro.local>
2016-12-06 11:48:47 +0800

    getter

--- UdeskSDK/UDChatMessage/UDIM/Views/Controller/UdeskTicketViewController.m
+++ UdeskSDK/UDChatMessage/UDIM/Views/Controller/UdeskTicketViewController.m
@@ -61,33 +61,31 @@
         CGRect webViewRect = self.navigationController.navigationBarHidden?CGRectMake(0, 64, UD_SCREEN_WIDTH, UD_SCREEN_HEIGHT-64):self.view.bounds;
         _ticketWebView = [[UIWebView alloc] initWithFrame:webViewRect];
         _ticketWebView.backgroundColor = [UIColor whiteColor];
-        
-        NSURL *ticketURL = (self.urlStr ? [NSURL URLWithString:self.urlStr] : [UdeskManager getSubmitTicketURL]);
 
-//        NSURL *ticketURL = nil;
-//        if (self.urlStr) {
-//            ticketURL = [NSURL URLWithString:self.urlStr];
-//        }else{
-//            ticketURL = [UdeskManager getSubmitTicketURL];
-//        }
+        NSURL *ticketURL = nil;
+        if (self.urlStr) {
+            ticketURL = [NSURL URLWithString:self.urlStr];
+        }else{
+            ticketURL =  [UdeskManager getSubmitTicketURL];
+           // 设置语言
+            NSString *tmp = [[NSUserDefaults standardUserDefaults] objectForKey:LANGUAGE_SET];
+           // 默认是中文
+            if (!tmp)
+            {
+                tmp = CNS;
+            }
+    
+            NSString *language;
+            if ([tmp isEqualToString:CNS]) {
+                language = @"&language=zh-cn";
+            }
+            else {
+                language = @"&language=en-us";
+            }
+            
+            ticketURL = [NSURL URLWithString:[ticketURL.absoluteString stringByAppendingString:language]];
+        }
 
-        //设置语言
-//        NSString *tmp = [[NSUserDefaults standardUserDefaults] objectForKey:LANGUAGE_SET];
-        //默认是中文
-//        if (!tmp)
-//        {
-//            tmp = CNS;
-//        }
-//        
-//        NSString *language;
-//        if ([tmp isEqualToString:CNS]) {
-//            language = @"&language=zh-cn";
-//        }
-//        else {
-//            language = @"&language=en-us";
-//        }
-//        
-//        ticketURL = [NSURL URLWithString:[ticketURL.absoluteString stringByAppendingString:language]];
 
         [_ticketWebView loadRequest:[NSURLRequest requestWithURL:ticketURL]];
         
@@ -161,10 +159,10 @@
 }
 
 
-#pragma mark - 留言自定义url
-- (NSString *)urlStr
-{
-    return @"https://www.baidu.com";
-}
+#pragma mark - 留言自定义url重写getter
+//- (NSString *)urlStr
+//{
+//    return @"https://www.baidu.com";
+//}
 
 @end
